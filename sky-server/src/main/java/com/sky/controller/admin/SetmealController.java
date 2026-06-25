ec2ee2eee57972b9c4b67cbd8f368b6cef575b7e
bb <3237688681@qq.com>
2026-06-25 15:25:48 +0800

    起售停售套餐业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -73,4 +73,16 @@ public class SetmealController {
         return Result.success();
     }
 
+    /**
+     * 套餐起售停售
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    public Result startOrStop(@PathVariable Integer status, Long id) {
+        setmealService.startOrStop(status, id);
+        return Result.success();
+    }
+
 }
