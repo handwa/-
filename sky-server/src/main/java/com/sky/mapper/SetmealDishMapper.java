41c21665a552b302f15d220116cb60efc08550f2
bb <3237688681@qq.com>
2026-06-25 11:14:32 +0800

    删除套餐业务代码开发

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 
 import com.sky.entity.SetmealDish;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -22,4 +23,11 @@ public interface SetmealDishMapper {
      */
 
     void insertBatch(List<SetmealDish> setmealDishes);
+
+    /**
+     * 根据套餐id删除套餐菜品数据
+     * @param setmealId
+     */
+    @Delete("delete from setmeal_dish where setmeal_id = #{setmealId}")
+    void deleteBySetmealId(Long setmealId);
 }
